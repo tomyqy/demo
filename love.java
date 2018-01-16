0fa4e8fb7d85962e43ee3e756003582842d74b6e
yqy <1563766861@qq.com>
2018-01-16 20:06:00 +0800

    爱心

---
+++ love.java
@@ -0,0 +1,25 @@
+package exe;
+
+public class love {
+
+   public static  void main(String args[]){
+           for(float y = (float) 1.5;y>-1.5;y -=0.1)
+           {
+               for(float x= (float) -1.5;x<1.5;x+= 0.05)
+               {
+                   float a = x*x+y*y-1;
+                   if((a*a*a-x*x*y*y*y)<=0.0)
+                   {
+
+
+                       System.out.print("*");
+
+                   }
+                   else
+                       System.out.print(" ");
+               }
+               System.out.print("\n");
+           }
+       }
+
+}
